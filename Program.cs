2fabfe16ec4c013e7fe629a493e579aafb33452d
Nikolay <niksan.nr@gmail.com>
2025-06-21 23:19:49 +0300

    Fixes #2

--- Program.cs
+++ Program.cs
@@ -39,6 +39,9 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(
+    name: "Dashboard",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
