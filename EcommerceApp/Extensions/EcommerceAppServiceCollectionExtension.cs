c01a4ae4c304c139cddc02308ffcd9e7ee1bccb6
Nikolay <niksan.nr@gmail.com>
2025-07-20 18:49:48 +0300

    Add "ProductService" to Dependency Injection

--- EcommerceApp/Extensions/EcommerceAppServiceCollectionExtension.cs
+++ EcommerceApp/Extensions/EcommerceAppServiceCollectionExtension.cs
@@ -1,5 +1,7 @@
 ﻿using EcommerceApp.Data.Repository;
 using EcommerceApp.Data.Repository.Interfaces;
+using EcommerceApp.Services.Data;
+using EcommerceApp.Services.Data.Interfaces;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
@@ -8,6 +10,7 @@ namespace Microsoft.Extensions.DependencyInjection
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
             services.AddScoped<IRepository, Repository>();
+            services.AddScoped<IProductService, ProductService>();
 
 			return services;
         }
