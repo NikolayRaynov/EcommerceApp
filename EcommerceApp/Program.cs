fbd52690c8e37fe67ba3f64bead5bc269efdda38
Nikolay <niksan.nr@gmail.com>
2025-07-24 21:27:31 +0300

    Change controller routing

--- EcommerceApp/Program.cs
+++ EcommerceApp/Program.cs
@@ -40,11 +40,11 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapControllerRoute(
     name: "areas",
     pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
