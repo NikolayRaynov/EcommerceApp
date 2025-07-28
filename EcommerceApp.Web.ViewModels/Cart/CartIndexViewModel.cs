5eb75357c1261617372690f36c683ff032b199f9
Nikolay <niksan.nr@gmail.com>
2025-07-28 19:08:22 +0300

    Add CartIndexViewModel

---
+++ EcommerceApp.Web.ViewModels/Cart/CartIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace EcommerceApp.Web.ViewModels.Cart
+{
+    public class CartIndexViewModel
+    {
+        public decimal ShippingCost { get; set; }
+        public decimal Subtotal => this.CartItems.Sum(item => item.LineTotal);
+        public decimal Total => this.Subtotal + this.ShippingCost;
+        public ICollection<CartItemViewModel> CartItems { get; set; } = new List<CartItemViewModel>();
+    }
+}
