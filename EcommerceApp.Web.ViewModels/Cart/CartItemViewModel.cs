522f7cca4a5bcce6a9e4a2794a2b41d794cfe190
Nikolay <niksan.nr@gmail.com>
2025-07-28 18:56:19 +0300

    Add CartItemViewModel

---
+++ EcommerceApp.Web.ViewModels/Cart/CartItemViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace EcommerceApp.Web.ViewModels.Cart
+{
+    public class CartItemViewModel
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; } = null!;
+        public string ImageUrl { get; set; } = null!;
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal LineTotal => this.Quantity * this.Price;
+    }
+}
