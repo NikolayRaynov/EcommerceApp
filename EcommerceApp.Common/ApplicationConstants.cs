6c980d6c045b2d2ebe3037884d010c07fff0f079
Nikolay <niksan.nr@gmail.com>
2025-08-22 15:38:50 +0300

    Add new constans for Application

--- EcommerceApp.Common/ApplicationConstants.cs
+++ EcommerceApp.Common/ApplicationConstants.cs
@@ -5,5 +5,7 @@
         public const int DefaultPageSize = 12;
         public const int DefaultPageNumber = 1;
         public const string DefaultImagePath = "wwwroot/images/Products";
+        public const decimal DefaultShippingCost = 5.00m;
+        public const int ReleaseYear = 2025;
     }
 }
