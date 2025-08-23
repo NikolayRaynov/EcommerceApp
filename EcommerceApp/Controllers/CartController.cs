3b06a38b485b0275f2df0110ec2b5b17ce94f01d
Nikolay <niksan.nr@gmail.com>
2025-08-23 15:46:19 +0300

    Add CartController

---
+++ EcommerceApp/Controllers/CartController.cs
@@ -0,0 +1,64 @@
+﻿using EcommerceApp.Services.Data.Interfaces;
+using EcommerceApp.Web.ViewModels.Cart;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace EcommerceApp.Controllers
+{
+    [Authorize]
+    public class CartController : Controller
+    {
+        private readonly ICartService cartService;
+        private readonly IProductService productService;
+
+        public CartController(ICartService cartService, IProductService productService)
+        {
+            this.cartService = cartService;
+            this.productService = productService;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Index()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var model = await this.cartService.GetUserCartAsync(userId);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddToCart(int productId)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            await this.cartService.AddProductToCartAsync(productId, userId);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> IncreaseQuantity(int id)
+        {
+            await this.cartService.IncreaseProductQuantityAsync(id);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DecreaseQuantity(int id)
+        {
+            await this.cartService.DecreaseProductQuantityAsync(id);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RemoveFromCart(int id)
+        {
+            await this.cartService.RemoveProductFromCartAsync(id);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
