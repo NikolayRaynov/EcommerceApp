f1a1f289d27b70c6f51590562797a5ca805cefbb
Nikolay <niksan.nr@gmail.com>
2025-08-23 14:36:45 +0300

    Add CartAddViewModel

---
+++ EcommerceApp.Web.ViewModels/Cart/CartAddViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static EcommerceApp.Common.EntityValidationConstants.Cart;
+using static EcommerceApp.Common.EntityValidationConstants.Product;
+
+namespace EcommerceApp.Web.ViewModels.Cart
+{
+    public class CartAddViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required]
+        [StringLength(CartItemMinQuantity, MinimumLength = CartItemMaxQuantity)]
+        public int Quantity { get; set; }
+
+        [Required]
+        [StringLength(ProductNameMaxLength, MinimumLength = ProductNameMinLength)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(ProductDescriptionMaxLength, MinimumLength = ProductDescriptionMinLength)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string UserId { get; set; } = null!;
+
+        [Required]
+        [MaxLength(ImageMaxLength)]
+        public string ImageUrl { get; set; } = string.Empty;
+    }
+}
