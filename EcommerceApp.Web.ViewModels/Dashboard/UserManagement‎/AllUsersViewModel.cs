538b90d6e68ab8862358e9ac1803ddbdc3838e1e
Nikolay <niksan.nr@gmail.com>
2025-08-31 17:42:41 +0300

    Add AllUsersViewModel

---
+++ EcommerceApp.Web.ViewModels/Dashboard/UserManagement‎/AllUsersViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EcommerceApp.WebViewModels.Dashboard.UserManagement
+{
+    public class AllUsersViewModel
+    {
+        public string Id { get; set; } = null!;
+
+        public string? Email { get; set; }
+
+        public IEnumerable<string> Roles { get; set; } = null!;
+    }
+}
