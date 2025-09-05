fe8b0d99e3e154af848af9f6a45e5da09a8f35ca
Nikolay <niksan.nr@gmail.com>
2025-09-05 21:23:49 +0300

    Add OrderCheckoutViewModel

---
+++ EcommerceApp.Web.ViewModels/Order/OrderCheckoutViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceApp.Web.ViewModels.Order
+{
+    public class OrderCheckoutViewModel
+    {
+        [Required]
+        [Display(Name = "Shipping Address")]
+        public string ShippingAddress { get; set; } = null!;
+        public string PaymentToken { get; set; } = null!;
+        public decimal Amount { get; set; }
+    }
+}
