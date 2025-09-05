aedcac0e4fd3b3e32a2953a0debfcc8d237d92a3
Nikolay <niksan.nr@gmail.com>
2025-09-05 21:49:41 +0300

    Add OrderDetailViewModel

---
+++ EcommerceApp.Web.ViewModels/Order/OrderDetailViewModel.cs
@@ -0,0 +1,14 @@
+﻿using EcommerceApp.Common.Enums;
+
+namespace EcommerceApp.Web.ViewModels.Order
+{
+    public class OrderDetailViewModel
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string ShippingAddress { get; set; } = null!;
+        public OrderStatus Status { get; set; }
+        public decimal TotalAmount { get; set; }
+        public ICollection<OrderItemViewModel> OrderItems { get; set; } = new List<OrderItemViewModel>();
+    }
+}
