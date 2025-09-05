acf6ef21cfd7901f89eb334c2d5d08d5b44b10c5
Nikolay <niksan.nr@gmail.com>
2025-09-05 21:42:18 +0300

    Add OrderItemViewModel

---
+++ EcommerceApp.Web.ViewModels/Order/OrderItemViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace EcommerceApp.Web.ViewModels.Order
+{
+    public class OrderItemViewModel
+    {
+        public string ProductName { get; set; } = null!;
+        public string Image { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal LineTotal { get; set; }
+    }
+}
