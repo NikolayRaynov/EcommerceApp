fc67955ee62eac56ee9e15ad6970b87e84bc8454
Nikolay <niksan.nr@gmail.com>
2025-09-06 17:07:38 +0300

    Add IOrderService interface

---
+++ EcommerceApp.Services.Data/Interfaces/IOrderService.cs
@@ -0,0 +1,12 @@
+﻿using EcommerceApp.Web.ViewModels.Order;
+
+namespace EcommerceApp.Services.Data.Interfaces
+{
+    public interface IOrderService
+    {
+        Task CreateOrderAsync(string userId, OrderCheckoutViewModel model);
+        Task<ICollection<OrderHistoryViewModel>> GetUserOrderAsync(string userId);
+        Task<OrderDetailViewModel> GetOrderDetailAsunc(int orderId, string userId);
+        Task<decimal> GetTotalAmountAsync(string userId);
+    }
+}
