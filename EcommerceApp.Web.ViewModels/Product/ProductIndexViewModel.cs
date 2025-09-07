9cb7f170ec595733156b11f20171110f3adbea4c
Nikolay <niksan.nr@gmail.com>
2025-09-07 16:47:27 +0300

    Add CreatedOn to ProductIndexViewModel

--- EcommerceApp.Web.ViewModels/Product/ProductIndexViewModel.cs
+++ EcommerceApp.Web.ViewModels/Product/ProductIndexViewModel.cs
@@ -7,5 +7,6 @@
         public string Description { get; set; } = string.Empty;
         public decimal Price { get; set; }
         public string Image { get; set; } = string.Empty;
+        public DateTime CreatedOn { get; set; }
     }
 }
