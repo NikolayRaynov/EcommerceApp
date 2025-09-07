86adcfd77fadbabe25b8a28fb335e59b5df4896a
Nikolay <niksan.nr@gmail.com>
2025-09-07 16:42:51 +0300

    Update AddProductViewModel

--- EcommerceApp.Web.ViewModels/Product/AddProductViewModel.cs
+++ EcommerceApp.Web.ViewModels/Product/AddProductViewModel.cs
@@ -23,6 +23,11 @@ namespace EcommerceApp.Web.ViewModels.Product
         public decimal Price { get; set; }
 
         [Required]
+        [MaxLength(ImageMaxLength)]
+        public string Image { get; set; } = string.Empty;
+
+        [Required]
+        [Comment("Date and time the product was added.")]
         public DateTime CreatedOn { get; set; }
 
         [Required]
