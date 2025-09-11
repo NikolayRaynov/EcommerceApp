4b08ac22c381503d1c564cc319e5a1d37e0b09b7
Nikolay <niksan.nr@gmail.com>
2025-09-11 21:39:56 +0300

    Add ProductPageViewModel

---
+++ EcommerceApp.Web.ViewModels/Product/ProductPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace EcommerceApp.Web.ViewModels.Product
+{
+    public class ProductPageViewModel
+    {
+        public int PageNumber { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalCount { get; set; }
+        public bool HasPreviousPage => PageNumber > 1;
+        public bool HasNextPage => PageNumber < TotalPages;
+        public List<ProductIndexViewModel> Products { get; set; }
+
+        public ProductPageViewModel()
+        {
+            Products = new List<ProductIndexViewModel>();
+        }
+    }
+}
