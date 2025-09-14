5514393ec20bb745c8cffafc5f77ee93818ec06e
Nikolay <niksan.nr@gmail.com>
2025-09-14 15:56:06 +0300

    Add Category model

---
+++ EcommerceApp.Data.Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static EcommerceApp.Common.EntityValidationConstants.Category;
+
+namespace EcommerceApp.Data.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(CategoryNameMaxLength)]
+        [MinLength(CategoryNameMinLength)]
+        [Comment("Name of the category.")]
+        public string Name { get; set; } = null!;
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+    }
+}
