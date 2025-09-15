46f3096fa0dee4c2cc85a4e3e76faca57c7ced98
Nikolay <niksan.nr@gmail.com>
2025-09-15 18:13:12 +0300

    Add EditCategoryViewModel

---
+++ EcommerceApp.Web.ViewModels/Category/EditCategoryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static EcommerceApp.Common.EntityValidationConstants.Category;
+
+namespace EcommerceApp.Web.ViewModels.Category
+{
+    public class EditCategoryViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The name of the category is required.")]
+        [StringLength(CategoryNameMaxLength, MinimumLength = CategoryNameMinLength,
+            ErrorMessage = "The name must be between {2} and {1} characters long.")]
+        public string Name { get; set; } = string.Empty;
+    }
+}
