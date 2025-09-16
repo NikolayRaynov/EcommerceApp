e208e537a5ae91a2b4829e829f49ec34976a1925
Nikolay <niksan.nr@gmail.com>
2025-09-16 18:37:18 +0300

    Implement FileService

---
+++ EcommerceApp.Services.Data/FileService.cs
@@ -0,0 +1,22 @@
+﻿using EcommerceApp.Services.Data.Interfaces;
+using System.Text.RegularExpressions;
+
+namespace EcommerceApp.Services.Data
+{
+    public class FileService : IFileService
+    {
+        public string SanitizeFolderName(string folderName)
+        {
+            char[] invalidFileNameChars = Path.GetInvalidFileNameChars();
+            char[] invalidPathChars = Path.GetInvalidPathChars();
+
+            string sanitized = new string(folderName
+                .Where(c => !invalidFileNameChars.Contains(c) && !invalidPathChars.Contains(c))
+                .ToArray());
+
+            sanitized = Regex.Replace(sanitized, @"[^a-zA-Z0-9_-]", "");
+
+            return sanitized;
+        }
+    }
+}
