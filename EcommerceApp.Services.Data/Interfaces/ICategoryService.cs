fbd3fe4c98a677c0d39f53aea15e571ecd4c6480
Nikolay <niksan.nr@gmail.com>
2025-09-16 18:49:56 +0300

    Add ICategoryService

---
+++ EcommerceApp.Services.Data/Interfaces/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using EcommerceApp.Web.ViewModels.Category;
+
+namespace EcommerceApp.Services.Data.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryViewModel>> GetAllAsync();
+        Task<CategoryViewModel?> GetByIdAsync(int id);
+        Task<EditCategoryViewModel?> GetCategoryForAsync(int id);
+        Task AddAsync(AddCategoryViewModel model, string webRootPath);
+        Task UpdateAsync(int id, EditCategoryViewModel model, string webRootPath);
+        Task<bool> ExistsByIdAsync(int id);
+        Task<string?> GetCategoryNameByIdAsync(int id);
+    }
+}
