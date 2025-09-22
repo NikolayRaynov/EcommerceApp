07c1668bd9614fbd9c062284d629e4108573490a
Nikolay <niksan.nr@gmail.com>
2025-09-22 15:04:03 +0300

    Implement custom error403 view

--- EcommerceApp/Controllers/HomeController.cs
+++ EcommerceApp/Controllers/HomeController.cs
@@ -61,11 +61,5 @@ namespace EcommerceApp.Controllers
 
             return this.View("Error500");
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
---
+++ EcommerceApp/wwwroot/js/error403.js
@@ -0,0 +1,9 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    lottie.loadAnimation({
+        container: document.getElementById('lottie-403'),
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'https://lottie.host/4e19ba39-c8a4-4a37-a3a4-5044966ce4b1/QsKGr2if0C.json'
+    });
+});
