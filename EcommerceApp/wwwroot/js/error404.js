1cc17029edf4e9e57d8884aac3330d7a0b16ed67
Nikolay <niksan.nr@gmail.com>
2025-09-22 15:44:11 +0300

    Implement custom error404 view

---
+++ EcommerceApp/wwwroot/js/error404.js
@@ -0,0 +1,9 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    lottie.loadAnimation({
+        container: document.getElementById('lottie-404'),
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'https://lottie.host/70cd2061-f7d2-443f-8496-4effcc411bee/nNRbsL23wH.json'
+    });
+});
