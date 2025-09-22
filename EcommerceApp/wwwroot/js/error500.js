fc02aea7d0cd2b929a8745f4f7aeb7a78c286f87
Nikolay <niksan.nr@gmail.com>
2025-09-22 15:59:42 +0300

    Implement custom error500 view

---
+++ EcommerceApp/wwwroot/js/error500.js
@@ -0,0 +1,9 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    lottie.loadAnimation({
+        container: document.getElementById('lottie-500'),
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'https://lottie.host/a2f22284-e727-4453-b661-54ed0b4c5c7c/suVYEc4hbo.json'
+    });
+});
