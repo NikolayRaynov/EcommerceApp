eab102e1a137ac7664c69e361a816aaf68bf56e8
Nikolay <niksan.nr@gmail.com>
2025-09-23 19:57:19 +0300

    Update IProductService

--- EcommerceApp.Services.Data/Interfaces/IProductService.cs
+++ EcommerceApp.Services.Data/Interfaces/IProductService.cs
@@ -1,4 +1,5 @@
 ﻿using EcommerceApp.Web.ViewModels.Product;
+using Microsoft.AspNetCore.Http;
 using static EcommerceApp.Common.ApplicationConstants;
 
 namespace EcommerceApp.Services.Data.Interfaces
@@ -7,9 +8,9 @@ namespace EcommerceApp.Services.Data.Interfaces
     {
         Task<ProductPageViewModel> GetAllProductsAsync(int pageNumber = DefaultPageNumber, 
             int pageSize = DefaultPageSize, string? searchTerm = null);
-        Task<int> AddProductAsync(AddProductViewModel model);
+        Task<int> AddProductAsync(AddProductViewModel model, List<IFormFile> images);
         Task<EditProductViewModel?> GetProductForEditAsync(int productId);
-        Task<bool> UpdateProductAsync(EditProductViewModel model);
+        Task<bool> UpdateProductAsync(EditProductViewModel model, List<IFormFile> newImages);
         Task<DeleteProductViewModel?> GetProductForDeleteAsync(int productId);
         Task<bool> DeleteProductAsync(int productId);
         Task<int> GetProductCountAsync(string? searchTerm = null);
@@ -17,6 +18,7 @@ namespace EcommerceApp.Services.Data.Interfaces
         Task<ProductPageViewModel> GetPopularProductsAsync(int pageNumber = DefaultPageNumber,
             int pageSize = DefaultPageSize);
         Task<ProductPageViewModel> GetNewArrivalsAsync(int pageNumber = DefaultPageNumber,
-            int pageSize = DefaultPageSize);
+            int pageSize = DefaultPageSize);    
+        Task DeleteImageAsync(int destinationId, string imageUrl);
     }
 }
