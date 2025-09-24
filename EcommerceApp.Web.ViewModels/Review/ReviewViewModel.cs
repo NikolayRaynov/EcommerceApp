464bd8182f8af16742f293a0edc93676c53259a1
Nikolay <niksan.nr@gmail.com>
2025-09-24 19:57:29 +0300

    Add ReviewViewModel

---
+++ EcommerceApp.Web.ViewModels/Review/ReviewViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace EcommerceApp.Web.ViewModels.Review
+{
+    public class ReviewViewModel
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int Rating { get; set; }
+        public string? Comment { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string User { get; set; } = string.Empty;
+    }
+}
