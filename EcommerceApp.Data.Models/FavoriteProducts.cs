064f9f05011d78f0910383b0905f635a49724888
Nikolay <niksan.nr@gmail.com>
2025-09-24 18:46:10 +0300

    Add FavoriteProducts model

---
+++ EcommerceApp.Data.Models/FavoriteProducts.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EcommerceApp.Data.Models
+{
+    public class FavoriteProducts
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Comment("Identifier of the user who marked the product as favorite.")]
+        public string UserId { get; set; } = string.Empty;
+        [ForeignKey(nameof(UserId))]
+        public virtual ApplicationUser User { get; set; } = null!;
+        public virtual ICollection<Product> Products { get; set; } = new HashSet<Product>();
+    }
+}
