e24416a1a84e3edb7fa030e7e660e5bf9613e1a5
Nikolay <niksan.nr@gmail.com>
2025-09-25 17:51:36 +0300

    Add AddReviewViewModel

---
+++ EcommerceApp.Web.ViewModels/Review/AddReviewViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace EcommerceApp.Web.ViewModels.Review
+{
+    using System.ComponentModel.DataAnnotations;
+    using static EcommerceApp.Common.EntityValidationConstants.Review;
+    public class AddReviewViewModel
+    {
+        public int ProductId { get; set; }
+
+        [Range(1, 5, ErrorMessage = "Рейтингът трябва да бъде между 1 и 5.")]
+        public int Rating { get; set; }
+
+        [MaxLength(CommentMaxLength)]
+        public string Comment { get; set; } = string.Empty;
+    }
+}
