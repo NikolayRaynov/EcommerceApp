45daf001b5912a0290232da2a6d43e71e375c343
Nikolay <niksan.nr@gmail.com>
2025-09-25 17:57:35 +0300

    Add DeleteReviewViewModel

---
+++ EcommerceApp.Web.ViewModels/Review/DeleteReviewViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace EcommerceApp.Web.ViewModels.Review
+{
+    using System.ComponentModel.DataAnnotations;
+    using static EcommerceApp.Common.EntityValidationConstants.Review;
+    public class DeleteReviewViewModel
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int Rating { get; set; }
+
+        [MaxLength(CommentMaxLength)]
+        public string? Comment { get; set; }
+    }
+}
