567eba47297776b4c0e6dd4fe618e720ccb652d4
Nikolay <niksan.nr@gmail.com>
2025-09-26 19:44:37 +0300

    Remove GetProductCountAsync from Interface

--- EcommerceApp.Services.Data/Interfaces/IProductService.cs
+++ EcommerceApp.Services.Data/Interfaces/IProductService.cs
@@ -7,18 +7,17 @@ namespace EcommerceApp.Services.Data.Interfaces
     public interface IProductService
     {
         Task<ProductPageViewModel> GetAllProductsAsync(int pageNumber = DefaultPageNumber, 
-            int pageSize = DefaultPageSize, string? searchTerm = null);
+            int pageSize = DefaultPageSize, int? categoryId = null);
         Task<int> AddProductAsync(AddProductViewModel model, List<IFormFile> images);
         Task<EditProductViewModel?> GetProductForEditAsync(int productId);
         Task<bool> UpdateProductAsync(EditProductViewModel model, List<IFormFile> newImages);
         Task<DeleteProductViewModel?> GetProductForDeleteAsync(int productId);
         Task<bool> DeleteProductAsync(int productId);
-        Task<int> GetProductCountAsync(string? searchTerm = null);
         Task<ProductIndexViewModel?> GetProductByIdAsync(int productId);
         Task<ProductPageViewModel> GetPopularProductsAsync(int pageNumber = DefaultPageNumber,
             int pageSize = DefaultPageSize);
         Task<ProductPageViewModel> GetNewArrivalsAsync(int pageNumber = DefaultPageNumber,
             int pageSize = DefaultPageSize);    
-        Task DeleteImageAsync(int destinationId, string imageUrl);
+        Task DeleteImageAsync(int productId, string imageUrl);
     }
 }
