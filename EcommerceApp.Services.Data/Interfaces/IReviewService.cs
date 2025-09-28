0af932d203a486a8c20e01cc804c89aa4ef71712
Nikolay <niksan.nr@gmail.com>
2025-09-28 13:43:53 +0300

    Add IReviewService

---
+++ EcommerceApp.Services.Data/Interfaces/IReviewService.cs
@@ -0,0 +1,13 @@
+﻿using EcommerceApp.Web.ViewModels.Review;
+
+namespace EcommerceApp.Services.Data.Interfaces
+{
+    public interface IReviewService
+    {
+        Task<IEnumerable<ReviewViewModel>> GetReviewsByProductIdAsync(int productId);
+        Task<ReviewViewModel?> GetReviewByIdAsync(int reviewId);
+        Task AddReviewAsync(AddReviewViewModel model, string userId);
+        Task UpdateReviewAsync(EditReviewViewModel model, string userId);
+        Task DeleteReviewAsync(DeleteReviewViewModel model, string userId);
+    }
+}
