9922518a15c758afa3c09fcae833089edbad3f43
Nikolay <niksan.nr@gmail.com>
2025-09-30 18:35:05 +0300

    Add AddToFavoriteViewModel

---
+++ EcommerceApp.Web.ViewModels/FavoriteProduct/AddToFavoriteViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceApp.Web.ViewModels.FavoritePlace
+{
+    using static EcommerceApp.Common.EntityValidationConstants.Product;
+    using static EcommerceApp.Common.EntityValidationConstants.Image;
+    public class AddToFavoriteViewModel
+    {
+        public int ProductId { get; set; }
+
+        [Required]
+        [MinLength(ProductNameMinLength)]
+        [MaxLength(ProductNameMaxLength)]
+        public string ProductName { get; set; } = null!;
+
+        [Required]
+        [Range(typeof(int), ProductPriceMinValue, ProductPriceMaxValue)]
+        public int ProductPrice { get; set; }
+
+        [Required]
+        [MinLength(ProductDescriptionMinLength)]
+        [MaxLength(ProductDescriptionMaxLength)]
+        public string ProductDescription { get; set; } = null!;
+
+        [MaxLength(ImageUrlMaxLength)]
+        public List<string> ProductImageUrls { get; set; } = new List<string>();
+    }
+}
