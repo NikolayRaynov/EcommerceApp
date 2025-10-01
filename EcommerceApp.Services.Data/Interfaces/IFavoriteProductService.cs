79fa43a11011564ee9e2612205f15e3ab5080101
Nikolay <niksan.nr@gmail.com>
2025-10-01 18:53:53 +0300

    Add IFavoriteProductService

---
+++ EcommerceApp.Services.Data/Interfaces/IFavoriteProductService.cs
@@ -0,0 +1,12 @@
+﻿using EcommerceApp.Web.ViewModels.Product;
+
+namespace EcommerceApp.Services.Data.Interfaces
+{
+    public interface IFavoriteProductService
+    {
+        Task<bool> AddToFavoritesAsync(int productId, string userId);
+        Task RemoveFromFavoritesAsync(int productId, string userId);
+        Task<bool> IsInFavoriteAsync(int productId, string userId);
+        Task<IEnumerable<ProductIndexViewModel>> GetAllFavoritesAsync(string userId);
+    }
+}
