8cccea0c1eb174e78172523f6e626d6c3657067e
Nikolay <niksan.nr@gmail.com>
2025-10-06 18:55:29 +0300

    Add ProductsByCategoryViewModel

---
+++ EcommerceApp.Web.ViewModels/Product/ProductsByCategoryViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace EcommerceApp.Web.ViewModels.Product
+{
+    public class ProductsByCategoryViewModel
+    {
+        public string CategoryName { get; set; } = null!;
+        public int ProductCount { get; set; }
+    }
+}
