e7402d2dde3f2172217a4f6b185e14b0dd0df586
Nikolay <niksan.nr@gmail.com>
2025-10-07 19:34:21 +0300

    Add ProductsLowStockQuantity view model

---
+++ EcommerceApp.Web.ViewModels/Product/ProductsLowStockQuantity.cs
@@ -0,0 +1,8 @@
+﻿namespace EcommerceApp.Web.ViewModels.Product
+{
+    public class ProductsLowStockQuantity
+    {
+        public string ProductName { get; set; } = null!;
+        public int StockQuantity { get; set; }
+    }
+}
