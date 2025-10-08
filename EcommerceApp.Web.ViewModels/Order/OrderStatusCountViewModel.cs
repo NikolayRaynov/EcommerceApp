7460743786f7ee0b88e7cca37b3e1894bfc5b6d6
Nikolay <niksan.nr@gmail.com>
2025-10-08 19:59:16 +0300

    Add OrderStatusCountViewModel view model

---
+++ EcommerceApp.Web.ViewModels/Order/OrderStatusCountViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace EcommerceApp.Web.ViewModels.Order
+{
+    public class OrderStatusCountViewModel
+    {
+        public string StatusName { get; set; } = null!;
+        public int Count { get; set; }
+    }
+}
