02ae6e6f9542bfa00af0f7c4cfea5e3adb9e777a
Nikolay <niksan.nr@gmail.com>
2025-10-08 20:04:54 +0300

    Add OrderStatusDistribution property to AdminDashboardViewModel

--- EcommerceApp.Web.ViewModels/Admin/AdminDashboardViewModel.cs
+++ EcommerceApp.Web.ViewModels/Admin/AdminDashboardViewModel.cs
@@ -6,7 +6,8 @@ namespace EcommerceApp.Web.ViewModels.Admin
     public class AdminDashboardViewModel
     {
         public int TotalOrders { get; set; }
-        public int TotalUsers { get; set; }
+        public IEnumerable<OrderStatusCountViewModel> OrderStatusDistribution { get; set; }
+            = new List<OrderStatusCountViewModel>();
         public IEnumerable<ProductsLowStockQuantity> LowStockQuantities { get; set; }
             = new List<ProductsLowStockQuantity>();
         public IEnumerable<ProductsByCategoryViewModel> ProductsByCategory { get; set; }
