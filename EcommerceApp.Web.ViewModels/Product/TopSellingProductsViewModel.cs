e6b9889caa236c8fd905f99645a130e539319e3d
Nikolay <niksan.nr@gmail.com>
2025-10-09 18:24:09 +0300

    Add TopSellingProductsViewModel

---
+++ EcommerceApp.Web.ViewModels/Product/TopSellingProductsViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace EcommerceApp.Web.ViewModels.Product
+{
+    public class TopSellingProductsViewModel
+    {
+        public string ProductName { get; set; } = null!;
+        public int QuantitySold { get; set; }
+    }
+}
