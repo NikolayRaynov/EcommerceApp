8345adbfb2a0ac5c138712f4a95221575bb8ae9c
Nikolay <niksan.nr@gmail.com>
2025-10-09 18:35:23 +0300

    Add TopSellingProducts to AdminDashboardViewModel

--- EcommerceApp.Web.ViewModels/Admin/AdminDashboardViewModel.cs
+++ EcommerceApp.Web.ViewModels/Admin/AdminDashboardViewModel.cs
@@ -5,7 +5,8 @@ namespace EcommerceApp.Web.ViewModels.Admin
 {
     public class AdminDashboardViewModel
     {
-        public int TotalOrders { get; set; }
+        public IEnumerable<TopSellingProductsViewModel> TopSellingProducts { get; set; }
+            = new List<TopSellingProductsViewModel>();
         public IEnumerable<OrderStatusCountViewModel> OrderStatusDistribution { get; set; }
             = new List<OrderStatusCountViewModel>();
         public IEnumerable<ProductsLowStockQuantity> LowStockQuantities { get; set; }
