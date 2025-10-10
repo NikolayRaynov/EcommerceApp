6d173fa36abf6785084481b27e069bc344e36a69
Nikolay <niksan.nr@gmail.com>
2025-10-10 19:20:09 +0300

    Update OrderCheckoutViewModel

--- EcommerceApp.Web.ViewModels/Order/OrderCheckoutViewModel.cs
+++ EcommerceApp.Web.ViewModels/Order/OrderCheckoutViewModel.cs
@@ -4,10 +4,20 @@ namespace EcommerceApp.Web.ViewModels.Order
 {
     public class OrderCheckoutViewModel
     {
-        [Required]
+        public string OrderId { get; set; } = null!;
+
+        [Display(Name = "Name")]
+        public string ProductName { get; set; } = null!;
+
+        [Display(Name = "Description")]
+        public string ProductDescription { get; set; } = null!;
+
         [Display(Name = "Shipping Address")]
         public string ShippingAddress { get; set; } = null!;
-        //public string PaymentToken { get; set; } = null!;
+
+        [Display(Name = "Total Amount")]
         public decimal TotalAmount { get; set; }
+        public string CurrencySymbol { get; set; } = "€";
+        public List<string> ProductImageUrls { get; set; } = new List<string>();
     }
 }
