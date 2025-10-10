35b00f90a2801810710a701e8eaa2e8749bab370
Nikolay <niksan.nr@gmail.com>
2025-10-10 19:16:55 +0300

    Add IStripeService interface

---
+++ EcommerceApp.Services.Data/Interfaces/IStripeService.cs
@@ -0,0 +1,9 @@
+﻿using EcommerceApp.Web.ViewModels.Order;
+
+namespace EcommerceApp.Services.Data.Interfaces
+{
+    public interface IStripeService
+    {
+        Task<string> CreateCheckoutSessionUrlAsync(OrderCheckoutViewModel checkoutViewModel);
+    }
+}
