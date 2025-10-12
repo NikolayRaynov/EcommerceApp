6ec2289e3f115dc51e87b03fa3505ba0e7d360b2
Nikolay <niksan.nr@gmail.com>
2025-10-12 10:17:44 +0300

    Update OrderItemViewModel

--- EcommerceApp.Web.ViewModels/Order/OrderItemViewModel.cs
+++ EcommerceApp.Web.ViewModels/Order/OrderItemViewModel.cs
@@ -3,10 +3,10 @@
     public class OrderItemViewModel
     {
         public string ProductName { get; set; } = null!;
-        public string Image { get; set; } = null!;
         public string Description { get; set; } = null!;
         public int Quantity { get; set; }
         public decimal Price { get; set; }
         public decimal LineTotal { get; set; }
+        public List<string> ImageUrls { get; set; } = new List<string>();
     }
 }
