66fb3d04efb65073f0d82d6cb822f486f901c90b
Nikolay <niksan.nr@gmail.com>
2025-10-12 10:16:38 +0300

    Update CartItemViewModel

--- EcommerceApp.Web.ViewModels/Cart/CartItemViewModel.cs
+++ EcommerceApp.Web.ViewModels/Cart/CartItemViewModel.cs
@@ -4,9 +4,9 @@
     {
         public int Id { get; set; }
         public string ProductName { get; set; } = null!;
-        public string ImageUrl { get; set; } = null!;
         public int Quantity { get; set; }
         public decimal Price { get; set; }
         public decimal LineTotal => this.Quantity * this.Price;
+        public List<string> ImageUrls { get; set; } = new List<string>();
     }
 }
