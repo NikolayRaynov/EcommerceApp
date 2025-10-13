003d723910479bee6778c5aedf9ac6604b14b307
Nikolay <niksan.nr@gmail.com>
2025-10-13 20:18:21 +0300

    Update OrderCheckoutViewModel

--- EcommerceApp.Web.ViewModels/Order/OrderCheckoutViewModel.cs
+++ EcommerceApp.Web.ViewModels/Order/OrderCheckoutViewModel.cs
@@ -1,23 +1,25 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using static EcommerceApp.Common.EntityValidationConstants.Order;
 
 namespace EcommerceApp.Web.ViewModels.Order
 {
     public class OrderCheckoutViewModel
     {
-        public string OrderId { get; set; } = null!;
+        public string? OrderId { get; set; }
 
-        [Display(Name = "Name")]
-        public string ProductName { get; set; } = null!;
-
-        [Display(Name = "Description")]
-        public string ProductDescription { get; set; } = null!;
-
+        [Required(ErrorMessage = "Shipping address is required.")]
+        [StringLength(OrderAddressMaxLength, MinimumLength = OrderAddressMinLength, 
+            ErrorMessage = "Address must be between 10 and 200 characters.")]
         [Display(Name = "Shipping Address")]
         public string ShippingAddress { get; set; } = null!;
 
         [Display(Name = "Total Amount")]
         public decimal TotalAmount { get; set; }
         public string CurrencySymbol { get; set; } = "€";
-        public List<string> ProductImageUrls { get; set; } = new List<string>();
+        public decimal Subtotal { get; set; }
+        public decimal ShippingCost { get; set; }
+        public int TotalItems => OrderItems.Sum(i => i.Quantity);
+
+        public List<OrderItemViewModel> OrderItems { get; set; } = new List<OrderItemViewModel>();
     }
 }
