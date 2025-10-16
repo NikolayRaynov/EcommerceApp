188ea04d1c1678b7eb335c1fa62f90dfdacc6c06
Nikolay <niksan.nr@gmail.com>
2025-10-16 19:21:32 +0300

    Change CreateOrderAsync return type to Task<int>

--- EcommerceApp.Services.Data/Interfaces/IOrderService.cs
+++ EcommerceApp.Services.Data/Interfaces/IOrderService.cs
@@ -6,7 +6,7 @@ namespace EcommerceApp.Services.Data.Interfaces
 {
     public interface IOrderService
     {
-        Task CreateOrderAsync(string userId, OrderCheckoutViewModel model);
+        Task<int> CreateOrderAsync(string userId, OrderCheckoutViewModel model);
         Task<ICollection<OrderHistoryViewModel>> GetUserOrderAsync(string userId);
         Task<OrderDetailViewModel> GetOrderDetailAsync(int orderId, string userId);
         Task<decimal> GetTotalAmountAsync(string userId);
