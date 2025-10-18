caa35c9b137fe58b763dff4948c4e6400d835a2d
Nikolay <niksan.nr@gmail.com>
2025-10-18 21:22:14 +0300

    Implement SaveProfilePictureAsync method

--- EcommerceApp.Services.Data/FileService.cs
+++ EcommerceApp.Services.Data/FileService.cs
@@ -1,10 +1,18 @@
 ﻿using EcommerceApp.Services.Data.Interfaces;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using System.Text.RegularExpressions;
 
 namespace EcommerceApp.Services.Data
 {
     public class FileService : IFileService
     {
+        private readonly IWebHostEnvironment environment;
+
+        public FileService(IWebHostEnvironment environment)
+        {
+            this.environment = environment;
+        }
         public string SanitizeFolderName(string folderName)
         {
             char[] invalidFileNameChars = Path.GetInvalidFileNameChars();
@@ -17,6 +25,26 @@ namespace EcommerceApp.Services.Data
             sanitized = Regex.Replace(sanitized, @"[^a-zA-Z0-9_-]", "");
 
             return sanitized;
+        }
+
+        public async Task<string> SaveProfilePictureAsync(IFormFile file, string userId)
+        {
+            string uploadsFolder = Path.Combine(environment.WebRootPath, "images", "profilePictures");
+            if (!Directory.Exists(uploadsFolder))
+            {
+                Directory.CreateDirectory(uploadsFolder);
+            }
+
+            string fileExtension = Path.GetExtension(file.FileName);
+            string uniqueFileName = $"user-{userId}{fileExtension}";
+            string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(fileStream);
+            }
+
+            return $"/images/profilePictures/{uniqueFileName}";
         }
     }
 }
--- EcommerceApp.Services.Data/Interfaces/IFileService.cs
+++ EcommerceApp.Services.Data/Interfaces/IFileService.cs
@@ -1,7 +1,10 @@
-﻿namespace EcommerceApp.Services.Data.Interfaces
+﻿using Microsoft.AspNetCore.Http;
+
+namespace EcommerceApp.Services.Data.Interfaces
 {
     public interface IFileService
     {
         string SanitizeFolderName(string folderName);
+        Task<string> SaveProfilePictureAsync(IFormFile file, string userId);
     }
 }
