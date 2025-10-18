53d7b67766084ccbe9b367e6d0cdef9f6e2527a8
Nikolay <niksan.nr@gmail.com>
2025-10-18 21:34:08 +0300

    Add "AddProfilePictureViewModel" view model

---
+++ EcommerceApp.Web.ViewModels/Manage/AddProfilePictureViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceApp.Web.ViewModels.Manage
+{
+    public class AddProfilePictureViewModel
+    {
+        [Required]
+        [DataType(DataType.Upload)]
+        [Display(Name = "Profile Picture")]
+        public IFormFile ProfileImage { get; set; } = null!;
+    }
+}
