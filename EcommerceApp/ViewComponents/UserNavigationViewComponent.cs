3b36c51fd582618fe896dcf236e9ff14b81521e5
Nikolay <niksan.nr@gmail.com>
2025-10-19 13:33:38 +0300

    Implement UserNavigationViewComponent

---
+++ EcommerceApp/ViewComponents/UserNavigationViewComponent.cs
@@ -0,0 +1,37 @@
+﻿using EcommerceApp.Data.Models;
+using EcommerceApp.Web.ViewModels.Manage;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using static EcommerceApp.Common.ApplicationConstants;
+
+namespace EcommerceApp.ViewComponents
+{
+    [ViewComponent(Name = "UserNavigation")]
+    public class UserNavigationViewComponent : ViewComponent
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public UserNavigationViewComponent(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return View(null);
+            }
+
+            string userId = userManager.GetUserId(UserClaimsPrincipal);
+            var user = await userManager.FindByIdAsync(userId);
+
+            var model = new ProfilePictureViewModel
+            {
+                ProfilePictureUrl = user?.ProfilePictureUrl ?? DefaultProfilePicturePath
+            };
+
+            return View(model);
+        }
+    }
+}
