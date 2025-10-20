9d84a3f831e72367c71711882ed91abcd6546658
Nikolay <niksan.nr@gmail.com>
2025-10-20 18:47:31 +0300

    Update ProfilePictureViewModel

--- EcommerceApp.Web.ViewModels/Manage/ProfilePictureViewModel.cs
+++ EcommerceApp.Web.ViewModels/Manage/ProfilePictureViewModel.cs
@@ -1,7 +1,15 @@
-﻿namespace EcommerceApp.Web.ViewModels.Manage
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceApp.Web.ViewModels.Manage
 {
     public class ProfilePictureViewModel
     {
         public string ProfilePictureUrl { get; set; } = null!;
+        public long ProfilePictureVersion { get; set; }
+
+        [DataType(DataType.Upload)]
+        [Display(Name = "Profile Picture")]
+        public IFormFile? ProfileImage { get; set; }
     }
 }
