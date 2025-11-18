244f7e2b601ab0d3aa1e0924a61a6ff02d0d3e2d
Nikolay <niksan.nr@gmail.com>
2025-11-18 20:35:47 +0200

    Add ContactViewModel

---
+++ EcommerceApp.Web.ViewModels/Home/ContactViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static EcommerceApp.Common.ApplicationConstants;
+
+namespace EcommerceApp.Web.ViewModels.Home
+{
+    public class ContactViewModel
+    {
+        [Required]
+        [MaxLength(MaxNameLength)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(MaxEmailLength)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(MaxMessageLength)]
+        public string Message { get; set; } = string.Empty;
+    }
+}
